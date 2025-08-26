821bf4caa9719cddeee9b71bc7fb60781dd9d006
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-08-26 20:04:13 -0300

    restaure

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'dragonite',
+            name: 'Magnemite',
             location: {
                 lat: -23.357913,
                 lng: -47.853324,
@@ -18,7 +18,8 @@ function staticLoadPlaces() {
                 lat: -23.357905,
                 lng: -47.853348,
             }
-        }
+        },
+        
     ];
 }
 
@@ -27,3 +28,28 @@ function staticLoadPlaces() {
 // -23.357777, -47.853325
 
 
+
+
+
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
